34206fec34085621a2f9f1e5d58dfd0f9865bbae
0xmarshall <0xmarshall@protonmail.com>
2023-07-21 16:41:00 +0100

    Added support for rabby wallet

--- src/config/constants/web3.ts
+++ src/config/constants/web3.ts
@@ -1,19 +1,41 @@
 import { ChainID, CHAINS } from "@/types";
-import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  coinbaseWallet,
+  injectedWallet,
+  metaMaskWallet,
+  rabbyWallet,
+  rainbowWallet,
+  walletConnectWallet
+} from '@rainbow-me/rainbowkit/wallets';
 import { configureChains, createConfig } from "wagmi";
 import { metis } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
-
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   CHAINS,
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: "",
-  // projectId: "YOUR_PROJECT_ID",
-  chains: chains,
-});
+// const { connectors } = getDefaultWallets({
+//   appName: "",
+//   // projectId: "YOUR_PROJECT_ID",
+//   chains: chains,
+// });
+
+const appName= "Unimaia Staker"
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Recommended',
+    wallets: [
+      metaMaskWallet({ chains }),
+      rainbowWallet({  chains }),
+      rabbyWallet({ chains }),
+      coinbaseWallet({appName, chains}),
+      walletConnectWallet({  chains }),
+      injectedWallet({ chains })
+    ],
+  },
+]);
 
 export const wagmiClient = createConfig({
   autoConnect: true,
