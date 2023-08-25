7dee5b0d6078d27fa0e419177fce8b5a9c279b22
Pavol Molcsan <p.molcsan@gmail.com>
2023-08-25 16:17:17 +0200

    chore(links): Updated urls to new unimaia

--- src/config/constants/nav.ts
+++ src/config/constants/nav.ts
@@ -1,39 +1,38 @@
-
 export type NavItem = {
-  title: string;
-  href?: string;
-  subItems?: NavItem[];
+	title: string
+	href?: string
+	subItems?: NavItem[]
 }
 
 export const NAV_ITEMS: NavItem[] = [
-  {
-    title: "Incentives",
-    href: "/",
-  },
-  {
-    title: "Positions",
-    href: "/rewards",
-  },
-  {
-    title: "More",
-    href: "/",
-    subItems: [
-      {
-        title: "Relevant links",
-        href: "https://linktr.ee/maiadao",
-      },
-      {
-        title: "Trade on UniMaia",
-        href: "https://uni.maiadao.io/#/swap",
-      },
-      {
-        title: "Trade on Hermes",
-        href: "https://hermes.maiadao.io/",
-      },
-      {
-        title: "Stake on Maia",
-        href: "https://maiadao.io/",
-      }
-    ]
-  }
-];
+	{
+		title: 'Incentives',
+		href: '/',
+	},
+	{
+		title: 'Positions',
+		href: '/rewards',
+	},
+	{
+		title: 'More',
+		href: '/',
+		subItems: [
+			{
+				title: 'Relevant links',
+				href: 'https://linktr.ee/maiadao',
+			},
+			{
+				title: 'Trade on UniMaia',
+				href: 'https://v3.maiadao.io/#/swap',
+			},
+			{
+				title: 'Trade on Hermes',
+				href: 'https://hermes.maiadao.io/',
+			},
+			{
+				title: 'Stake on Maia',
+				href: 'https://maiadao.io/',
+			},
+		],
+	},
+]
